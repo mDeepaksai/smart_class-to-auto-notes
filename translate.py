e14b4591f928589ea70ad26f346ab915364a3839
mDeepaksai <mdeepaksai806@gmail.com>
2026-02-08 20:26:27 +0530

    trying

--- translate.py
+++ translate.py
@@ -1,9 +1,17 @@
 import whisper
 import os
+import sys
 
-model = whisper.load_model("base")
+sys.stdout.reconfigure(encoding="utf-8")
 
-audio_path = os.path.join("temp", "aaa.mp3")
+audio_path = r"C:\Users\mdeep\Downloads\voiceaudio\parrin .mp3"
+
+print("Audio path:", audio_path)
+print("Exists:", os.path.exists(audio_path))
+
+if not os.path.exists(audio_path):
+    raise FileNotFoundError("Audio file not found")
+
+model = whisper.load_model("turbo")
 result = model.transcribe(audio_path)
-
 print(result["text"])
